990418d1c9493a3bba05f0a5c08b63c5d577ab2f
Abdallah-Helal <abdallahelal102@gmail.com>
2023-03-28 05:59:01 +0200

    adding Result Component

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -1,29 +1,34 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import data from "../database/data";
 export default function Questions({ onChecked }) {
   const [checked, setChecked] = useState(undefined);
-
+  const question = data[0];
+  useEffect(() => {
+    console.log(data);
+  });
   function onSelect(i) {
     console.log("radio button change ");
   }
 
   return (
     <div className="questions">
-      <h2 className="text-light">Simple Question 1</h2>
-      <ul>
-        <li>
-          <input
-            type="radio"
-            value={false}
-            name="options"
-            id={`q1-option`}
-            onChange={() => onSelect()}
-          />
-          <label className="text-primary" htmlFor="q1-options">
-            options
-          </label>
-          <div className="check checked "></div>
-        </li>
+      <h2 className="text-light">{question.question}</h2>
+      <ul key={question.id}>
+        {question.options.map((q, i) => (
+          <li key={i}>
+            <input
+              type="radio"
+              value={false}
+              name="options"
+              id={`q${i}-option`}
+              onChange={() => onSelect()}
+            />
+            <label className="text-primary" htmlFor={`q${i}-option`}>
+              {q}
+            </label>
+            <div className="check  "></div>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -1,5 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "../styles/Result.css";
+import { Link } from "react-router-dom";
+import ResultTable from "./ResultTable";
 
 export default function Result() {
-  return <div>Result</div>;
+  function onRestart() {
+    console.log("on Restart");
+  }
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application</h1>
+
+      <div className="result flex-center">
+        <div className="flex">
+          <span>Username</span>
+          <span className="bold">{"userId" || ""}</span>
+        </div>
+        <div className="flex">
+          <span>Total Quiz Points : </span>
+          <span className="bold">{"totalPoints" || 0}</span>
+        </div>
+        <div className="flex">
+          <span>Total Questions : </span>
+          <span className="bold">{"queue.length" || 0}</span>
+        </div>
+        <div className="flex">
+          <span>Total Attempts : </span>
+          <span className="bold">{"attempts" || 0}</span>
+        </div>
+        <div className="flex">
+          <span>Total Earn Points : </span>
+          <span className="bold">{"earnPoints" || 0}</span>
+        </div>
+        <div className="flex">
+          <span>Quiz Result</span>
+          <span
+            style={{ color: `${"flag" ? "#2aff95" : "#ff2a66"}` }}
+            className="bold"
+          >
+            {"flag" ? "Passed" : "Failed"}
+          </span>
+        </div>
+      </div>
+
+      <div className="start">
+        <Link className="btn" to={"/"} onClick={onRestart}>
+          Restart
+        </Link>
+      </div>
+
+      <div className="container">
+        {/* result table */}
+        <ResultTable></ResultTable>
+      </div>
+    </div>
+  );
 }
