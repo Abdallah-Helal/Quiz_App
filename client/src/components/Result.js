ebb7236751dfcb1bd83859b066062fd93f174a73
Abdallah-Helal <abdallahelal102@gmail.com>
2023-04-06 06:09:59 +0200

    adding ,reducers ,Dispatch ,Store Values, Moving to Next Question, Push Result

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -1,11 +1,20 @@
 import React, { useEffect } from "react";
 import "../styles/Result.css";
 import { Link } from "react-router-dom";
+
 import ResultTable from "./ResultTable";
+import { useDispatch, useSelector } from "react-redux";
+
+/** import actions  */
+import { resetAllAction } from "../redux/question_reducer";
+import { resetResultAction } from "../redux/result_reducer";
 
 export default function Result() {
+  const dispatch = useDispatch();
+
   function onRestart() {
-    console.log("on Restart");
+    dispatch(resetAllAction());
+    dispatch(resetResultAction());
   }
   return (
     <div className="container">
